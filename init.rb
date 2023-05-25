0ea60debea13d76314053da1d6cbf87cdd69d713
Anna Born <anna.born@dfki.de>
2023-05-25 20:28:53 +0200

    add complite list of ros2 osdeps

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
-Autoproj.config.declare("ros2_version",
-    "string",
-    default: "foxy",
-    possible_values: ["foxy","humble"],
-    doc: ["Which ros2 version? [foxy, humble]"])
+#Autoproj.config.declare("ros2_version",
+#    "string",
+#    default: "foxy",
+#    possible_values: ["foxy","humble"],
+#    doc: ["Which ros2 version? [foxy, humble]"])
 
-if Autoproj.config.get("ros2_version") == "foxy" || Autoproj.config.get("ros2_version") == "humble"
-    Autoproj.message ("Load workspace suffix " + Autoproj.config.get("ros2_version"))
-    Autoproj.workspace.osdep_suffixes << Autoproj.config.get("ros2_version")
-end
+#if Autoproj.config.get("ros2_version") == "foxy" || Autoproj.config.get("ros2_version") == "humble"
+#    Autoproj.message ("Load workspace suffix " + Autoproj.config.get("ros2_version"))
+#    Autoproj.workspace.osdep_suffixes << Autoproj.config.get("ros2_version")
+#end

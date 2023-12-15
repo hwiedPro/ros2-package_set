930dbdba64c407b623a323ab968aecc9ead6e48b
Steffen Planthaber <steffen.planthaber@dfki.de>
2023-12-15 10:26:32 +0100

    fix colcon package

--- lib/colcon_package.rb
+++ lib/colcon_package.rb
@@ -1,8 +1,11 @@
 
-def colcon_package(name, type = :import_package, workspace: Autoproj.workspace)
+def colcon_package(name, type = :import_package, move_to: name, workspace: Autoproj.workspace)
     send(type, name) do |pkg|
-        pkg.pkg.use_package_xml = true
+        pkg.use_package_xml = true
         yield(pkg) if block_given?
+    end
+    if name != move_to then
+        move_package name, move_to
     end
 end
 

f5e4a05268f0960edc0b6d7c5a0a0244de5113ad
Anna Born <anna.born@yardstick-robotics.com>
2024-04-04 11:40:18 +0200

    add dependency to colcon

--- lib/colcon_package.rb
+++ lib/colcon_package.rb
@@ -1,6 +1,7 @@
 
 def colcon_package(name, type = :import_package, move_to: name, workspace: Autoproj.workspace)
     send(type, name) do |pkg|
+        pkg.depends_on 'python3-colcon-common-extensions'
         pkg.use_package_xml = true
         yield(pkg) if block_given?
     end

99e1e292af9255c31a9382e95a6f0596747ad97c
AB <banonran@gmx.de>
2024-04-05 10:03:52 +0200

    Update colcon_package.rb

--- lib/colcon_package.rb
+++ lib/colcon_package.rb
@@ -1,5 +1,6 @@
 def colcon_package(name, type = :import_package, move_to: name, workspace: Autoproj.workspace)
     send(type, name) do |pkg|
+        pkg.depends_on 'python3-colcon-common-extensions'
         pkg.use_package_xml = true
         # allows to enable tests and install the dependencies
         # you have to run autoproj test enable

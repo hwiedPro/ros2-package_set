a869ae67b6135234250f85cb06826841a2c399a5
Anna Born <anna.born@yardstick-robotics.com>
2024-04-05 09:57:25 +0200

    add test block to enable the tests

--- lib/colcon_package.rb
+++ lib/colcon_package.rb
@@ -1,14 +1,16 @@
-
 def colcon_package(name, type = :import_package, move_to: name, workspace: Autoproj.workspace)
     send(type, name) do |pkg|
-        pkg.depends_on 'python3-colcon-common-extensions'
         pkg.use_package_xml = true
+        # allows to enable tests and install the dependencies
+        # you have to run autoproj test enable
+        pkg.test_utility.no_results = true
+        pkg.test_utility.task do
+        end
         yield(pkg) if block_given?
     end
     if name != move_to then
         move_package name, move_to
     end
 end
-
 
 

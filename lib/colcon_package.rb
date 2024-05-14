045a0a299ccc13877d0aac48f53e3bc2daa67e97
Steffen Planthaber <steffen.planthaber@dfki.de>
2024-05-14 16:02:53 +0200

    add support for multi-package repos (gazebo_ros2_control)

--- lib/colcon_package.rb
+++ lib/colcon_package.rb
@@ -1,13 +1,13 @@
 
-def colcon_package(name, type = :import_package, move_to: name, workspace: Autoproj.workspace)
+def colcon_package(name, type = :import_package, importdir: name, move_to: name, workspace: Autoproj.workspace)
     send(type, name) do |pkg|
         pkg.use_package_xml = true
+        if importdir != name then
+            pkg.importdir = importdir
+        end
         yield(pkg) if block_given?
     end
     if name != move_to then
         move_package name, move_to
     end
 end
-
-
-
